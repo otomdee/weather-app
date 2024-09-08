6207da6fecb6a39fc472498ac8553d4904251a35
otomdee <otomdaniel65@gmail.com>
2024-09-08 20:51:06 +0100

    add humidity and windspeed

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 async function fetchWeather(place) {
     try {
-        const weatherData = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}/2024-09-08T01:00:00?unitGroup=us&key=ED6AJWCEPSXB49HY494NGEYMQ&contentType=json&include=current`, {
+        const today = new Date();
+        const formattedDate = today.toISOString().split('T')[0];
+
+        const weatherData = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}/${formattedDate}T01:00:00?unitGroup=metric&key=ED6AJWCEPSXB49HY494NGEYMQ&contentType=json&include=current`, {
         mode: 'cors'});
         const weatherJson = await weatherData.json();
-        
+
         const icon = weatherJson.currentConditions.icon;
         const temp = weatherJson.days[0].temp;
         const location = weatherJson.resolvedAddress;
+        const humidity = weatherJson.currentConditions.humidity;
+        const windspeed = weatherJson.currentConditions.windspeed;
 
         return {
             icon: icon,
             temp: temp,
-            location: location
+            location: location,
+            humidity: humidity,
+            windspeed: windspeed
         }
     }
     catch(error) {
@@ -48,10 +55,14 @@ function render(object) {
    weatherIcon.append(icon);
 
    const tempSpan = document.createElement("span");
-   tempSpan.textContent = object.temp;
+   tempSpan.textContent = `${object.temp}\u00B0c`;
    temp.append(tempSpan);
 
    const locationSpan = document.createElement("span");
    locationSpan.textContent = object.location;
    location.append(locationSpan);
+
+   document.querySelector("#humidityData").textContent = `${object.humidity}%`;
+
+   document.querySelector("#windData").textContent = `${object.windspeed}km/h`;
 }
